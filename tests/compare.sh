01094a9d4f49bb5520907a69bfb2e7984c449e9f
Lauren Toy <ltoy@oicr.on.ca>
2022-09-21 11:54:27 -0400

    try other method  Please enter the commit message for your changes. Lines starting

--- tests/compare.sh
+++ tests/compare.sh
@@ -3,6 +3,6 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-diff -s <(sort $1) <(sort $2)
-#diff $1 $2 
+#diff -s <(sort $1) <(sort $2)
+diff $1 $2 
 #from https://bitbucket.oicr.on.ca/projects/GSI/repos/spb-seqware-production/browse/vidarr/workflow-template/compare.sh
